d1c1b0098ae170d92de7805f28537e5e285352da
joaohneri <joaohnsousa@gmail.com>
2025-06-23 14:33:06 -0300

    Feat: inserção de novos endpoints de homologação e produção

--- src/config/sefaz-endpoints.ts
+++ src/config/sefaz-endpoints.ts
@@ -11,6 +11,18 @@ export const ENDPOINTS_HOMOLOGACAO: Record<string, SefazEndpoints> = {
     nfceConsulta: 'https://homologacao.nfce.fazenda.sp.gov.br/ws/NFeConsultaProtocolo4.asmx',
     nfceStatusServico: 'https://homologacao.nfce.fazenda.sp.gov.br/ws/NFeStatusServico4.asmx',
     nfceCancelamento: 'https://homologacao.nfce.fazenda.sp.gov.br/ws/NFeRecepcaoEvento4.asmx'
+  },
+  'PR': {
+    nfceAutorizacao: 'https://homologacao.nfce.sefa.pr.gov.br/nfce/NFeAutorizacao4',
+    nfceConsulta: 'https://homologacao.nfce.sefa.pr.gov.br/nfce/NFeConsultaProtocolo4',
+    nfceStatusServico: 'https://homologacao.nfce.sefa.pr.gov.br/nfce/NFeStatusServico4',
+    nfceCancelamento: 'https://homologacao.nfce.sefa.pr.gov.br/nfce/NFeRecepcaoEvento4'
+  },
+  'RS': {
+    nfceAutorizacao: 'https://nfce-homologacao.sefazrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao4.asmx',
+    nfceConsulta: 'https://nfce-homologacao.sefazrs.rs.gov.br/ws/NfeConsulta/NfeConsulta4.asmx',
+    nfceStatusServico: 'https://nfce-homologacao.sefazrs.rs.gov.br/ws/NfeStatusServico/NfeStatusServico4.asmx',
+    nfceCancelamento: 'https://nfce-homologacao.sefazrs.rs.gov.br/ws/recepcaoevento/recepcaoevento4.asmx'
   }
 };
 
@@ -20,5 +32,17 @@ export const ENDPOINTS_PRODUCAO: Record<string, SefazEndpoints> = {
     nfceConsulta: 'https://nfce.fazenda.sp.gov.br/ws/NFeConsultaProtocolo4.asmx',
     nfceStatusServico: 'https://nfce.fazenda.sp.gov.br/ws/NFeStatusServico4.asmx',
     nfceCancelamento: 'https://nfce.fazenda.sp.gov.br/ws/NFeRecepcaoEvento4.asmx'
+  },
+  'PR': {
+    nfceAutorizacao: 'https://nfce.sefa.pr.gov.br/nfce/NFeAutorizacao4',
+    nfceConsulta: 'https://nfce.sefa.pr.gov.br/nfce/NFeConsultaProtocolo4',
+    nfceStatusServico: 'https://nfce.sefa.pr.gov.br/nfce/NFeStatusServico4',
+    nfceCancelamento: 'https://nfce.sefa.pr.gov.br/nfce/NFeRecepcaoEvento4'
+  },
+  'RS': {
+    nfceAutorizacao: 'https://nfce.sefazrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao4.asmx',
+    nfceConsulta: 'https://nfce.sefazrs.rs.gov.br/ws/NfeConsulta/NfeConsulta4.asmx',
+    nfceStatusServico: 'https://nfce.sefazrs.rs.gov.br/ws/NfeStatusServico/NfeStatusServico4.asmx',
+    nfceCancelamento: 'https://nfce.sefazrs.rs.gov.br/ws/recepcaoevento/recepcaoevento4.asmx'
   }
 };
